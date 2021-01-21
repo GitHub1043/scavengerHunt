6087118925972c8aa12f8df2499922581777da5c
AYDEN HOLGATE <AYDEN.HOLGATE@connect.7oaks.org>
2021-01-21 14:13:17 -0600

    Initial commit

---
+++ scavengerHunt/Form1.Designer.vb
@@ -0,0 +1,230 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.chkDinosaur = New System.Windows.Forms.CheckBox()
+        Me.chkPizza = New System.Windows.Forms.CheckBox()
+        Me.chkDog = New System.Windows.Forms.CheckBox()
+        Me.chkPaper = New System.Windows.Forms.CheckBox()
+        Me.chkPicture = New System.Windows.Forms.CheckBox()
+        Me.chkPhone = New System.Windows.Forms.CheckBox()
+        Me.chkLight = New System.Windows.Forms.CheckBox()
+        Me.lblScore = New System.Windows.Forms.Label()
+        Me.btnScore = New System.Windows.Forms.Button()
+        Me.radTwo = New System.Windows.Forms.RadioButton()
+        Me.radThree = New System.Windows.Forms.RadioButton()
+        Me.radFour = New System.Windows.Forms.RadioButton()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.chkLight)
+        Me.GroupBox1.Controls.Add(Me.chkPhone)
+        Me.GroupBox1.Controls.Add(Me.chkPicture)
+        Me.GroupBox1.Controls.Add(Me.chkPaper)
+        Me.GroupBox1.Controls.Add(Me.chkDog)
+        Me.GroupBox1.Controls.Add(Me.chkPizza)
+        Me.GroupBox1.Controls.Add(Me.chkDinosaur)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(317, 272)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Items"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.radFour)
+        Me.GroupBox2.Controls.Add(Me.radThree)
+        Me.GroupBox2.Controls.Add(Me.radTwo)
+        Me.GroupBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.Location = New System.Drawing.Point(466, 12)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(317, 272)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Players"
+        '
+        'chkDinosaur
+        '
+        Me.chkDinosaur.AutoSize = True
+        Me.chkDinosaur.Location = New System.Drawing.Point(6, 25)
+        Me.chkDinosaur.Name = "chkDinosaur"
+        Me.chkDinosaur.Size = New System.Drawing.Size(87, 22)
+        Me.chkDinosaur.TabIndex = 0
+        Me.chkDinosaur.Text = "Dinosaur"
+        Me.chkDinosaur.UseVisualStyleBackColor = True
+        '
+        'chkPizza
+        '
+        Me.chkPizza.AutoSize = True
+        Me.chkPizza.Location = New System.Drawing.Point(6, 60)
+        Me.chkPizza.Name = "chkPizza"
+        Me.chkPizza.Size = New System.Drawing.Size(64, 22)
+        Me.chkPizza.TabIndex = 1
+        Me.chkPizza.Text = "Pizza"
+        Me.chkPizza.UseVisualStyleBackColor = True
+        '
+        'chkDog
+        '
+        Me.chkDog.AutoSize = True
+        Me.chkDog.Location = New System.Drawing.Point(6, 95)
+        Me.chkDog.Name = "chkDog"
+        Me.chkDog.Size = New System.Drawing.Size(55, 22)
+        Me.chkDog.TabIndex = 2
+        Me.chkDog.Text = "Dog"
+        Me.chkDog.UseVisualStyleBackColor = True
+        '
+        'chkPaper
+        '
+        Me.chkPaper.AutoSize = True
+        Me.chkPaper.Location = New System.Drawing.Point(6, 130)
+        Me.chkPaper.Name = "chkPaper"
+        Me.chkPaper.Size = New System.Drawing.Size(66, 22)
+        Me.chkPaper.TabIndex = 3
+        Me.chkPaper.Text = "Paper"
+        Me.chkPaper.UseVisualStyleBackColor = True
+        '
+        'chkPicture
+        '
+        Me.chkPicture.AutoSize = True
+        Me.chkPicture.Location = New System.Drawing.Point(6, 165)
+        Me.chkPicture.Name = "chkPicture"
+        Me.chkPicture.Size = New System.Drawing.Size(73, 22)
+        Me.chkPicture.TabIndex = 4
+        Me.chkPicture.Text = "Picture"
+        Me.chkPicture.UseVisualStyleBackColor = True
+        '
+        'chkPhone
+        '
+        Me.chkPhone.AutoSize = True
+        Me.chkPhone.Location = New System.Drawing.Point(6, 200)
+        Me.chkPhone.Name = "chkPhone"
+        Me.chkPhone.Size = New System.Drawing.Size(70, 22)
+        Me.chkPhone.TabIndex = 5
+        Me.chkPhone.Text = "Phone"
+        Me.chkPhone.UseVisualStyleBackColor = True
+        '
+        'chkLight
+        '
+        Me.chkLight.AutoSize = True
+        Me.chkLight.Location = New System.Drawing.Point(6, 235)
+        Me.chkLight.Name = "chkLight"
+        Me.chkLight.Size = New System.Drawing.Size(58, 22)
+        Me.chkLight.TabIndex = 6
+        Me.chkLight.Text = "Light"
+        Me.chkLight.UseVisualStyleBackColor = True
+        '
+        'lblScore
+        '
+        Me.lblScore.AutoSize = True
+        Me.lblScore.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblScore.Location = New System.Drawing.Point(462, 353)
+        Me.lblScore.Name = "lblScore"
+        Me.lblScore.Size = New System.Drawing.Size(107, 20)
+        Me.lblScore.TabIndex = 10
+        Me.lblScore.Text = "Total Score: 0"
+        '
+        'btnScore
+        '
+        Me.btnScore.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnScore.Location = New System.Drawing.Point(200, 339)
+        Me.btnScore.Name = "btnScore"
+        Me.btnScore.Size = New System.Drawing.Size(129, 34)
+        Me.btnScore.TabIndex = 11
+        Me.btnScore.Text = "Calculate Score"
+        Me.btnScore.UseVisualStyleBackColor = True
+        '
+        'radTwo
+        '
+        Me.radTwo.AutoSize = True
+        Me.radTwo.Location = New System.Drawing.Point(6, 25)
+        Me.radTwo.Name = "radTwo"
+        Me.radTwo.Size = New System.Drawing.Size(108, 22)
+        Me.radTwo.TabIndex = 0
+        Me.radTwo.TabStop = True
+        Me.radTwo.Text = "Two Players"
+        Me.radTwo.UseVisualStyleBackColor = True
+        '
+        'radThree
+        '
+        Me.radThree.AutoSize = True
+        Me.radThree.Location = New System.Drawing.Point(6, 93)
+        Me.radThree.Name = "radThree"
+        Me.radThree.Size = New System.Drawing.Size(117, 22)
+        Me.radThree.TabIndex = 1
+        Me.radThree.TabStop = True
+        Me.radThree.Text = "Three Players"
+        Me.radThree.UseVisualStyleBackColor = True
+        '
+        'radFour
+        '
+        Me.radFour.AutoSize = True
+        Me.radFour.Location = New System.Drawing.Point(6, 161)
+        Me.radFour.Name = "radFour"
+        Me.radFour.Size = New System.Drawing.Size(110, 22)
+        Me.radFour.TabIndex = 2
+        Me.radFour.TabStop = True
+        Me.radFour.Text = "Four Players"
+        Me.radFour.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 448)
+        Me.Controls.Add(Me.btnScore)
+        Me.Controls.Add(Me.lblScore)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents chkLight As System.Windows.Forms.CheckBox
+    Friend WithEvents chkPhone As System.Windows.Forms.CheckBox
+    Friend WithEvents chkPicture As System.Windows.Forms.CheckBox
+    Friend WithEvents chkPaper As System.Windows.Forms.CheckBox
+    Friend WithEvents chkDog As System.Windows.Forms.CheckBox
+    Friend WithEvents chkPizza As System.Windows.Forms.CheckBox
+    Friend WithEvents chkDinosaur As System.Windows.Forms.CheckBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents lblScore As System.Windows.Forms.Label
+    Friend WithEvents btnScore As System.Windows.Forms.Button
+    Friend WithEvents radFour As System.Windows.Forms.RadioButton
+    Friend WithEvents radThree As System.Windows.Forms.RadioButton
+    Friend WithEvents radTwo As System.Windows.Forms.RadioButton
+
+End Class
---
+++ scavengerHunt/Form1.vb
@@ -0,0 +1,70 @@
+﻿Public Class Form1
+
+    Private Sub btnScore_Click(sender As Object, e As EventArgs) Handles btnScore.Click
+        Randomize()
+
+        Const TWO As Integer = 2
+        Const THREE As Integer = 3
+        Const FOUR As Integer = 4
+        Dim intItems As Integer
+        Dim intScore As Integer
+        Dim intBonus As Integer
+
+        If chkDinosaur.Checked = True Then
+            intItems = intItems + 1
+        End If
+        If chkPizza.Checked = True Then
+            intItems = intItems + 1
+        End If
+        If chkDog.Checked = True Then
+            intItems = intItems + 1
+        End If
+        If chkPaper.Checked = True Then
+            intItems = intItems + 1
+        End If
+        If chkPicture.Checked = True Then
+            intItems = intItems + 1
+        End If
+        If chkPhone.Checked = True Then
+            intItems = intItems + 1
+        End If
+        If chkLight.Checked = True Then
+            intItems = intItems + 1
+        End If
+
+        If radTwo.Checked = True Then
+            intScore = (5 - TWO) * intItems
+        ElseIf radThree.Checked = True Then
+            intScore = (5 - THREE) * intItems
+        ElseIf radFour.Checked = True Then
+            intScore = (5 - FOUR) * intItems
+        End If
+
+        intBonus = Int(Rnd() * 7) + 1
+
+        If intBonus = 1 And chkDinosaur.Checked = True Then
+            intScore = intScore + 5
+            MessageBox.Show("You Found The Seceret Item!")
+        ElseIf intBonus = 2 And chkPizza.Checked = True Then
+            intScore = intScore + 5
+            MessageBox.Show("You Found The Seceret Item!")
+        ElseIf intBonus = 3 And chkDog.Checked = True Then
+            intScore = intScore + 5
+            MessageBox.Show("You Found The Seceret Item!")
+        ElseIf intBonus = 4 And chkPaper.Checked = True Then
+            intScore = intScore + 5
+            MessageBox.Show("You Found The Seceret Item!")
+        ElseIf intBonus = 5 And chkPicture.Checked = True Then
+            intScore = intScore + 5
+            MessageBox.Show("You Found The Seceret Item!")
+        ElseIf intBonus = 6 And chkPhone.Checked = True Then
+            intScore = intScore + 5
+            MessageBox.Show("You Found The Seceret Item!")
+        ElseIf intBonus = 7 And chkLight.Checked = True Then
+            intScore = intScore + 5
+            MessageBox.Show("You Found The Seceret Item!")
+        End If
+
+        lblScore.Text = "Total Score: " & intScore
+    End Sub
+End Class
